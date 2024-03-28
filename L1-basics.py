22891daea405fb530b76459f798c3f9f79a2f7bb
Mansi Balram Handi <152275064+mansihandi10@users.noreply.github.com>
2024-03-28 10:51:53 +0530

    Add files via upload

---
+++ L1-basics.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 08:43:49 2024
+
+@author: HP
+"""
+#print Hello World
+print("Hello World...\nMansi is Here...")
+"""
+.py is the extention which contain one or more python statemnets 
+that represents a python program or script.
+"""
+#variables
+"""
+there are three types of numbers used to represent numbers in python 
+these are 1)integer
+          2)floating numbers 
+          3)complex numbers
+"""
+#for integers
+x=10
+print(x)#10
+print(type(x))#'<class 'int'>'
+
+#for floating numbers
+y=10.12
+print(y)#10.12
+print(type(y))#'<class 'float'>'
+
